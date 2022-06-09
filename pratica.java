91a87486ffbdd0a38154f8cc302333f8d3ac3a37
miguelarcanjo1 <102737532+miguelarcanjo1@users.noreply.github.com>
2022-06-09 11:03:33 -0300

    Add files via upload

---
+++ pratica.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class pratica {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Seu numero");
+        double nota1 = entrada.nextDouble();
+        System.out.println("Seu numero" + nota1); 
+        
+       
+        System.out.println("Seu numero");
+        double nota2 = entrada.nextDouble();
+        System.out.println("Seu numero" + nota2); 
+        
+
+        //atv2
+       // public static void main(String[] args){
+            Scanner entrada = new Scanner(System.in);
+            
+            System.out.println("Digite um valor em metros");
+            double valor = entrada.nextDouble();
+            System.out.println("" + valor); 
+            
+           
+            double resul = ((valor)/100);
+            System.out.println("Em centimetro deu : " + resul);
+            
+    
+            //2
+
+
+    }
+}
